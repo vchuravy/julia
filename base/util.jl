80c16f15df7b3dfc46777ce3fea880e1e2bad331
Elliot Saba <staticfloat@gmail.com>
2014-06-04 16:06:00 -0700

    Add Base.build_sysimg(), a julia function to force a rebuild of the system image, given the existance of a supported compiler

--- base/util.jl
+++ base/util.jl
@@ -204,3 +204,71 @@ end
 
 warn(err::Exception; prefix="ERROR: ", kw...) =
     warn(sprint(io->showerror(io,err)), prefix=prefix; kw...)
+
+
+# By default, put the system image next to libjulia
+build_sysimg(;force=false) = build_sysimg(joinpath(dirname(Sys.dlpath("libjulia")),"sys"), force=force)
+
+# Build a system image binary at sysimg_path.dlext.  If a system image is already loaded, error out, or continue if force = true
+function build_sysimg(sysimg_path; force=false)
+    # Unless force == true, quit out if a sysimg is already loadable
+    sysimg = dlopen_e("sys")
+    if !force && sysimg != C_NULL
+        println("System image already loaded at $(Sys.dlpath(sysimg)), pass \"force=true\" to override")
+        return;
+    end
+
+    # Enter base/ and setup some useful paths
+    cd(dirname(Base.find_source_file("sysimg.jl")))
+    julia = joinpath(JULIA_HOME, "julia")
+    julia_libdir = dirname(Sys.dlpath("libjulia"))
+    cxx = find_cxx_compiler()
+
+    # Ensure we have write-permissions to wherever we're trying to write to
+    if !success(`touch $sysimg_path.$(Sys.dlext)`)
+        error("$sysimg_path unwritable, ensure parent directory exists and is writable! (Do you need to run this with sudo?)")
+    end
+
+    # Start by building sys0.{ji,o}
+    sys0_path = joinpath(dirname(sysimg_path), "sys0")
+    println("Building sys0.o...")
+    run(`$julia --build $sys0_path sysimg.jl`)
+
+    # Bootstrap off of that to create sys.{ji,o}
+    println("Building sys.o...")
+    run(`$julia --build $sysimg_path -J $sys0_path.ji -f sysimg.jl`)
+
+    # Link sys.o into sys.$(dlext)
+    FLAGS = ["-shared", "-fPIC", "-L$julia_libdir"]
+    if OS_NAME == :Darwin
+        push!(FLAGS, "-Wl,-undefined,dynamic_lookup")
+    else
+        push!(FLAGS, "-Wl,--unresolved-symbols,ignore-all")
+    end
+    @windows_only append!(FLAGS, ["-L$JULIA_HOME", "-ljulia", "-lssp"])
+
+    println("Linking sys.$(Sys.dlext)")
+    run(`$cxx $FLAGS -o $sysimg_path.$(Sys.dlext) $sysimg_path.o`)
+end
+
+# Search for a C++ compiler to link sys.o into sys.dl_ext.  Honor CXX environment variable, otherwise search for something we know works
+function find_cxx_compiler()
+    if haskey( ENV, "CXX" )
+        if !success(`$(ENV["CXX"]) --version`)
+            warn("Using compiler override $(ENV["CXX"]), but unable to run `$(ENV["CXX"]) --version`")
+        end
+        return ENV["CXX"]
+    end
+
+    poss_compilers = ["clang++", "g++"]
+
+    for compiler in poss_compilers
+        try
+            if success(`$compiler --version`)
+                return compiler
+            end
+        end
+    end
+
+    error( "No supported compilers found (tried $(join(poss_compilers, ", "))), override with CXX environment variable!" )
+end
